e516286ef73efae392c060584789b151396f4453
aditi-goyal2 <aditi.1923csi1024@kiet.edu>
2021-06-22 19:09:12 +0530

    updated tha 9

---
+++ THA 9/script.js
@@ -0,0 +1,28 @@
+const main=document.querySelector(".main");
+const booked=document.querySelector(".booked");
+const Remaining=document.querySelector(".Remaining");
+var totalseats= 400;
+var bookedseats=0;
+for(let i=0;i<300;i++)
+{
+    const items=document.createElement("div")
+    items.addEventListener('click',()=>{
+        if(items.style.backgroundColor!="black"){
+            items.style.backgroundColor="black";
+            bookedseats+=1;
+            totalseats-=1;
+            booked.innerHTML = `${bookedseats}`;
+            Remaining.innerHTML = `<b>${totalseats}</b>`;
+        }
+        
+        else{
+            items.style.background="brown";
+            bookedseats-=1;
+            totalseats+=1;
+            booked.innerHTML = `${bookedseats}`;
+            Remaining.innerHTML = `<b>${totalseats}</b>`;
+        }
+        
+    })
+    main.appendChild(items)
+}
