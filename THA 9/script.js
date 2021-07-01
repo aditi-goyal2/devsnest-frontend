b5c75a7c4faf9711405b38d5f6c9bfe996c4fedb
aditi-goyal2 <aditi.1923csi1024@kiet.edu>
2021-07-01 13:12:09 +0530

    updated todo list

--- THA 9/script.js
+++ THA 9/script.js
@@ -1,11 +1,12 @@
 const main=document.querySelector(".main");
 const booked=document.querySelector(".booked");
 const Remaining=document.querySelector(".Remaining");
-var totalseats= 300;
+var totalseats= 56;
 var bookedseats=0;
-for(let i=0;i<300;i++)
+for(let i=0;i<56;i++)
 {
     const items=document.createElement("div")
+    // items.innerHTML="<i class='fas fa-chair fa-3x'></i>";
     items.addEventListener('click',()=>{
         if(items.style.backgroundColor!="black"){
             items.style.backgroundColor="black";
@@ -16,7 +17,7 @@ for(let i=0;i<300;i++)
         }
         
         else{
-            items.style.background="brown";
+            items.style.background="none rgba(94, 89, 89, 0.6)";
             bookedseats-=1;
             totalseats+=1;
             booked.innerHTML = `${bookedseats}`;
