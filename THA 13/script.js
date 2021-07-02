6c086d48eadbabec7021908ae99a3eb0115b2652
aditi-goyal2 <aditi.1923csi1024@kiet.edu>
2021-07-02 23:59:29 +0530

    addeed tha 13

---
+++ THA 13/script.js
@@ -0,0 +1,24 @@
+let num_memes=document.querySelector('#input_field')
+let memes_background=document.querySelector('.memes');
+
+function show(){
+   let numbers=num_memes.value;
+    console.log(numbers);
+    num_memes.value="";
+    // fetch(`https://meme-api.herokuapp.com/gimme/wholesomememes/${numbers}`)
+    // .then(
+    //     data => console.log(data)
+    // )
+   memes_background.innerHTML=""
+    fetch(`https://meme-api.herokuapp.com/gimme/wholesomememes/${numbers}`)
+        .then(res => res.json())
+        .then(data => {
+            data.memes.forEach((memes) => {
+                let src = "" + memes.preview[2]
+                let image = document.createElement("img")
+                image.src = src
+               memes_background.appendChild(image)
+            })
+        })
+        .catch(error => console.log(error))
+}
